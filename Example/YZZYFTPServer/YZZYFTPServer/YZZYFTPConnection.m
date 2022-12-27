f21f5c9d0faf1a3553956bb650bd84adb1f0fff0
ShengTianqi <shengtianqi@100tal.com>
2022-12-27 18:06:45 +0800

    添加Connection文件

---
+++ Example/YZZYFTPServer/YZZYFTPServer/YZZYFTPConnection.m
@@ -0,0 +1,55 @@
+//
+//  YZZYFTPConnection.m
+//  YZZYFTPServer_Example
+//
+//  Created by 云中追月 on 2022/12/27.
+//  Copyright © 2022 stqyzzy. All rights reserved.
+//
+
+#import "YZZYFTPConnection.h"
+
+@interface YZZYFTPConnection()
+
+@end
+
+@implementation YZZYFTPConnection
+
+#pragma mark -
+#pragma mark - life cycle - 生命周期
+- (void)dealloc{
+    NSLog(@"%@ - dealloc", NSStringFromClass([self class]));
+}
+
+- (instancetype)init{
+    if (self = [super init]) {
+        [self setup];
+    }
+    return self;
+}
+
+#pragma mark -
+#pragma mark - init setup - 初始化
+- (void)setup{
+    [self setDefault];//初始化默认数据
+}
+
+/// 设置默认数据
+- (void)setDefault{
+    
+}
+
+#pragma mark -
+#pragma mark - public methods
+
+
+#pragma mark -
+#pragma mark - <#custom#> Delegate
+
+#pragma mark -
+#pragma mark - private methods
+
+#pragma mark -
+#pragma mark - getters and setters
+
+
+@end
