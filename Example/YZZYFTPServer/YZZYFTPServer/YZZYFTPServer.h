6965b103ed9e4604f9f0af4a572573f744579863
ShengTianqi <shengtianqi@100tal.com>
2022-12-27 15:52:36 +0800

    新建文件

--- Example/YZZYFTPServer/YZZYFTPServer/YZZYFTPServer.h
+++ Example/YZZYFTPServer/YZZYFTPServer/YZZYFTPServer.h
@@ -6,11 +6,17 @@
 //  Copyright © 2022 stqyzzy. All rights reserved.
 //
 
+/*===================================================
+        * 文件描述 ：<#文件功能描述必写#> *
+=====================================================*/
+
 #import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface YZZYFTPServer : NSObject
+// 初始化方法，Dir是可以通讯的文件路径
+- (id)initWithPort:(unsigned)serverPort withDir:(NSString*)aDirectory notifyObject:(id)sender;
 
 @end
 
--- Example/YZZYFTPServer/YZZYFTPServer/YZZYFTPServer.m
+++ Example/YZZYFTPServer/YZZYFTPServer/YZZYFTPServer.m
@@ -8,6 +8,52 @@
 
 #import "YZZYFTPServer.h"
 
+@interface YZZYFTPServer()
+
+@end
+
 @implementation YZZYFTPServer
+
+#pragma mark -
+#pragma mark - life cycle - 生命周期
+- (void)dealloc{
+    NSLog(@"%@ - dealloc", NSStringFromClass([self class]));
+}
+
+- (instancetype)init{
+    if (self = [super init]) {
+        [self setup];
+    }
+    return self;
+}
+
+#pragma mark -
+#pragma mark - init setup - 初始化
+- (void)setup{
+    [self setDefault];//初始化默认数据
+}
+
+/// 设置默认数据
+- (void)setDefault{
+    
+}
+
+#pragma mark -
+#pragma mark - public methods
+// 初始化方法，Dir是可以通讯的文件路径
+- (id)initWithPort:(unsigned)serverPort withDir:(NSString*)aDirectory notifyObject:(id)sender {
+    
+}
+
+
+#pragma mark -
+#pragma mark - <#custom#> Delegate
+
+#pragma mark -
+#pragma mark - private methods
+
+#pragma mark -
+#pragma mark - getters and setters
+
 
 @end
