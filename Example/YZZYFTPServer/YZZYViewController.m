18e2011a156480948beb4d4be1608979e9742147
ShengTianqi <shengtianqi@100tal.com>
2022-12-27 17:10:12 +0800

    完成demo

--- Example/YZZYFTPServer/YZZYViewController.m
+++ Example/YZZYFTPServer/YZZYViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "YZZYViewController.h"
+#import "YZZYFTPServer.h"
 
 @interface YZZYViewController ()
-
+@property (nonatomic, strong) YZZYFTPServer *ftpServer;
+@property (nonatomic, assign) NSUInteger ftpPort;
 @end
 
 @implementation YZZYViewController
@@ -18,6 +20,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.ftpServer = [[YZZYFTPServer alloc] initWithPort:self.ftpPort withDir:NSHomeDirectory() notifyObject:self];
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +29,7 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (NSUInteger)ftpPort {
+    return 23333;
+}
 @end
