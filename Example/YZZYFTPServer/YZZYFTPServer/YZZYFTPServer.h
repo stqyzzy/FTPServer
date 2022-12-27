5c00f361d3554c5125a7ef1efc70eeb0cfd9b1bf
ShengTianqi <shengtianqi@100tal.com>
2022-12-27 15:48:28 +0800

    添加Server文件

---
+++ Example/YZZYFTPServer/YZZYFTPServer/YZZYFTPServer.h
@@ -0,0 +1,17 @@
+//
+//  YZZYFTPServer.h
+//  YZZYFTPServer_Example
+//
+//  Created by 云中追月 on 2022/12/27.
+//  Copyright © 2022 stqyzzy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YZZYFTPServer : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/YZZYFTPServer/YZZYFTPServer/YZZYFTPServer.m
@@ -0,0 +1,13 @@
+//
+//  YZZYFTPServer.m
+//  YZZYFTPServer_Example
+//
+//  Created by 云中追月 on 2022/12/27.
+//  Copyright © 2022 stqyzzy. All rights reserved.
+//
+
+#import "YZZYFTPServer.h"
+
+@implementation YZZYFTPServer
+
+@end
