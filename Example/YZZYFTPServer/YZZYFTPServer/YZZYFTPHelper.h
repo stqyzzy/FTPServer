966b2c722049bfd48443c38dbd1f81679edcb7fc
ShengTianqi <shengtianqi@100tal.com>
2023-01-10 17:48:42 +0800

    增加YZZYFTPHelper文件

---
+++ Example/YZZYFTPServer/YZZYFTPServer/YZZYFTPHelper.h
@@ -0,0 +1,21 @@
+//
+//  YZZYFTPHelper.h
+//  YZZYFTPServer_Example
+//
+//  Created by 云中追月 on 2023/1/10.
+//  Copyright © 2023 stqyzzy. All rights reserved.
+//
+
+/*===================================================
+        * 文件描述 ：<#文件功能描述必写#> *
+=====================================================*/
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YZZYFTPHelper : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/YZZYFTPServer/YZZYFTPServer/YZZYFTPHelper.m
@@ -0,0 +1,55 @@
+//
+//  YZZYFTPHelper.m
+//  YZZYFTPServer_Example
+//
+//  Created by 云中追月 on 2023/1/10.
+//  Copyright © 2023 stqyzzy. All rights reserved.
+//
+
+#import "YZZYFTPHelper.h"
+
+@interface YZZYFTPHelper()
+
+@end
+
+@implementation YZZYFTPHelper
+
+#pragma mark -
+#pragma mark - life cycle - 生命周期
+- (void)dealloc{
+    NSLog(@"%@ - dealloc", NSStringFromClass([self class]));
+}
+
+- (instancetype)init{
+    if (self = [super init]) {
+        [self setup];
+    }
+    return self;
+}
+
+#pragma mark -
+#pragma mark - init setup - 初始化
+- (void)setup{
+    [self setDefault];//初始化默认数据
+}
+
+/// 设置默认数据
+- (void)setDefault{
+    
+}
+
+#pragma mark -
+#pragma mark - public methods
+
+
+#pragma mark -
+#pragma mark - <#custom#> Delegate
+
+#pragma mark -
+#pragma mark - private methods
+
+#pragma mark -
+#pragma mark - getters and setters
+
+
+@end
