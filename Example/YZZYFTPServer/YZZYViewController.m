445ff916f05152bfdd5a4dc94562190f1f79b847
ShengTianqi <shengtianqi@100tal.com>
2023-01-31 14:19:22 +0800

    完成测试代码的编写

--- Example/YZZYFTPServer/YZZYViewController.m
+++ Example/YZZYFTPServer/YZZYViewController.m
@@ -8,8 +8,9 @@
 
 #import "YZZYViewController.h"
 #import "YZZYFTPServer.h"
-
+#import "YZZYFTPHelper.h"
 @interface YZZYViewController ()
+@property (nonatomic, strong) UILabel *addressInfoLabel;
 @property (nonatomic, strong) YZZYFTPServer *ftpServer;
 @property (nonatomic, assign) NSUInteger ftpPort;
 @end
@@ -20,7 +21,36 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.addressInfoLabel.text = [NSString stringWithFormat:@"ftp://%@:%u", [YZZYFTPHelper localIPAddress], self.ftpPort];
+    
+    [self generateTestFiles];
+    
     self.ftpServer = [[YZZYFTPServer alloc] initWithPort:self.ftpPort withDir:NSHomeDirectory() notifyObject:self];
+}
+
+- (void)generateTestFiles {
+    NSDateFormatter *fmt = [[NSDateFormatter alloc] init];
+    NSDate *nowDate = [NSDate date];
+    
+    // Documents文件夹
+    fmt.dateFormat = @"YY-MM-dd HH:mm:ss";
+    NSString *docTextString = [NSString stringWithFormat:@"documents text generate by XMFTPServer at %@", [fmt stringFromDate:nowDate]];
+    fmt.dateFormat = @"YYYYMMddHHmmss";
+    NSString *documentsFolderPathSthring = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
+    NSString *filePathString = [documentsFolderPathSthring stringByAppendingPathComponent:[NSString stringWithFormat:@"xmftp_doc_test_files_%@", [fmt stringFromDate:nowDate]]];
+    [docTextString writeToFile:filePathString atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    
+    // Library
+    fmt.dateFormat = @"YYYY-MM-dd HH:mm:ss";
+    NSString *libText = [NSString stringWithFormat:@"library text generate by XMFTPServer at %@", [fmt stringFromDate:nowDate]];
+    fmt.dateFormat = @"YYYYMMddHHmmss";
+    [libText writeToFile:[[NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) firstObject]stringByAppendingPathComponent:[NSString stringWithFormat:@"xmftp_lib_test_files_%@", [fmt stringFromDate:nowDate]]] atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    
+    // tmp
+    fmt.dateFormat = @"YYYY-MM-dd HH:mm:ss";
+    NSString *tmpText = [NSString stringWithFormat:@"tmp text generate by XMFTPServer at %@", [fmt stringFromDate:nowDate]];
+    fmt.dateFormat = @"YYYYMMddHHmmss";
+    [tmpText writeToFile:[NSTemporaryDirectory() stringByAppendingPathComponent:[NSString stringWithFormat:@"xmftp_tmp_test_files_%@", [fmt stringFromDate:nowDate]]] atomically:YES encoding:NSUTF8StringEncoding error:nil];
 }
 
 - (void)didReceiveMemoryWarning
@@ -31,5 +61,14 @@
 
 - (NSUInteger)ftpPort {
     return 23333;
+}
+
+- (UILabel *)addressInfoLabel {
+    if (_addressInfoLabel == nil) {
+        _addressInfoLabel = [[UILabel alloc] init];
+        _addressInfoLabel.frame = CGRectMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2, 250, 100);
+        [self.view addSubview:_addressInfoLabel];
+    }
+    return _addressInfoLabel;
 }
 @end
